b644bd0824e01a9482cd6b4e0fca4038009b427c
qwukker <minor7addfloortom@gmail.com>
2023-08-15 15:14:44 -0500

    aider: Added three boolean command-line arguments to `main.py`.
    # Context:
    USER: add three boolean cli arguments to main.py. 'chill', 'mid', and 'chad'.
    ASSISTANT: To add three boolean command-line arguments to `main.py`, we will use the `argparse` module from Python's standard library. This module makes it easy to write user-friendly command-line interfaces.
    
    Here are the steps:
    
    1. Import the `argparse` module.
    2. Create a parser object.
    3. Add the 'chill', 'mid', and 'chad' arguments to the parser.
    4. Parse the arguments.
    
    Here is the code change:
    
    ```python
    py_diffbro/main.py
    <<<<<<< ORIGINAL
    from modules.llm import prompt
    from modules.git import get_git_diff
    
    # here's a comment to test the git diff
    def main():
        git_diff = get_git_diff()
    
        print("git_diff", git_diff)
    
    if __name__ == "__main__":
        main()
    =======
    import argparse
    from modules.llm import prompt
    from modules.git import get_git_diff
    
    # here's a comment to test the git diff
    def main():
        parser = argparse.ArgumentParser()
        parser.add_argument('--chill', action='store_true')
        parser.add_argument('--mid', action='store_true')
        parser.add_argument('--chad', action='store_true')
        args = parser.parse_args()
    
        git_diff = get_git_diff()
    
        print("git_diff", git_diff)
        print("chill", args.chill)
        print("mid", args.mid)
        print("chad", args.chad)
    
    if __name__ == "__main__":
        main()
    >>>>>>> UPDATED
    ```
    
    With these changes, you can now use the 'chill', 'mid', and 'chad' arguments when running your script from the command line, like so: `python main.py --chill --mid --chad`. The script will print `True` for each argument that is present and `False` for each argument that is not.

--- py_diffbro/main.py
+++ py_diffbro/main.py
@@ -1,13 +1,22 @@
+import argparse
 from modules.llm import prompt
 from modules.git import get_git_diff
 
-
 # here's a comment to test the git diff
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--chill', action='store_true')
+    parser.add_argument('--mid', action='store_true')
+    parser.add_argument('--chad', action='store_true')
+    args = parser.parse_args()
+
     git_diff = get_git_diff()
 
     print("git_diff", git_diff)
-
+    print("chill", args.chill)
+    print("mid", args.mid)
+    print("chad", args.chad)
 
 if __name__ == "__main__":
+    main()
     main()
