65344639da46886256e3c67e248c9e6fcf664f07
qwukker <minor7addfloortom@gmail.com>
2023-08-20 17:58:31 -0500

    aider: Updated get_git_diff to support new flags for filtering files in the git diff command.
    # Context:
    USER: update git.py:get_git_diff to support it's new flags. If the 'only' list has items. exclusively diff those items. be sure to prefix each item with **/* so it's a recursive search. If only is empty and ignore is not. Include all items minus the ignore list.
    ASSISTANT: To support the new flags in `get_git_diff`, we need to modify the `py_diffbro/modules/git.py` file. We will update the `get_git_diff` function to dynamically build the `git diff` command based on the `only` and `ignore` lists.
    
    Here are the changes:
    
    ```python
    py_diffbro/modules/git.py
    <<<<<<< ORIGINAL
    def get_git_diff(only: List[str], ignore: List[str]):
        command = "git diff -- '**/*.py'"  # need to make this dynamic
        return run_shell_command(command)
    =======
    def get_git_diff(only: List[str], ignore: List[str]):
        if only:
            files = ' '.join(f"'**/*{ext}'" for ext in only)
            command = f"git diff -- {files}"
        elif ignore:
            files = ' '.join(f"'**/*{ext}'" for ext in ignore)
            command = f"git diff -- . ':{files}'"
        else:
            command = "git diff"
        return run_shell_command(command)
    >>>>>>> UPDATED
    ```
    
    In the updated code, if the `only` list has items, we build a string of file extensions to include in the `git diff` command. If the `only` list is empty and the `ignore` list is not, we build a string of file extensions to exclude from the `git diff` command. If both lists are empty, we simply run `git diff` without any file extension filters.

--- py_diffbro/modules/git.py
+++ py_diffbro/modules/git.py
@@ -3,5 +3,12 @@ from .cmd import run_shell_command
 
 
 def get_git_diff(only: List[str], ignore: List[str]):
-    command = "git diff -- '**/*.py'"  # need to make this dynamic
+    if only:
+        files = ' '.join(f"'**/*{ext}'" for ext in only)
+        command = f"git diff -- {files}"
+    elif ignore:
+        files = ' '.join(f"'**/*{ext}'" for ext in ignore)
+        command = f"git diff -- . ':{files}'"
+    else:
+        command = "git diff"
     return run_shell_command(command)
