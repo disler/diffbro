1362f13bcd467fe585ab27995049fe5697be9ef7
qwukker <minor7addfloortom@gmail.com>
2023-09-03 12:18:45 -0500

    aider: Add optional 'branch' argument to get_git_diff and modify command variable accordingly.
    # Context:
    USER: update get_git_diff to optionally take a new string 'branch' as an arg. Then update the command variable to include the branch only if it is set otherwise run it as is. For example if branch='main' and files='.ts' we should have a command that looks like 'git diff main -- **/*.ts'
    ASSISTANT: To add a new optional argument 'branch' to the `get_git_diff()` function and modify the `command` variable accordingly, we need to modify the `py_diffbro/modules/git.py` file.
    
    Here are the steps:
    
    1. Add a new optional argument 'branch' to the `get_git_diff()` function.
    2. Modify the `command` variable to include the branch if it is set.
    
    Here are the changes:
    
    ```python
    py_diffbro/modules/git.py
    <<<<<<< ORIGINAL
    def get_git_diff(only: List[str], ignore: List[str]):
    =======
    def get_git_diff(only: List[str], ignore: List[str], branch: str = None):
    >>>>>>> UPDATED
    ```
    
    ```python
    py_diffbro/modules/git.py
    <<<<<<< ORIGINAL
        command = f"git diff -- . {files}"
    =======
        command = f"git diff {branch + ' -- ' if branch else ''}. {files}"
    >>>>>>> UPDATED
    ```
    
    ```python
    py_diffbro/modules/git.py
    <<<<<<< ORIGINAL
        command = f"git diff -- {files}"
    =======
        command = f"git diff {branch + ' -- ' if branch else ''}{files}"
    >>>>>>> UPDATED
    ```
    
    ```python
    py_diffbro/modules/git.py
    <<<<<<< ORIGINAL
        command = "git diff"
    =======
        command = f"git diff {branch if branch else ''}"
    >>>>>>> UPDATED
    ```
    
    Please note that the `main.py` file needs to be updated to pass the 'branch' argument to the `get_git_diff()` function. If that file is read-only, please make it read-write so I can propose the necessary changes.

--- py_diffbro/modules/git.py
+++ py_diffbro/modules/git.py
@@ -2,7 +2,7 @@ from typing import List
 from .cmd import run_shell_command
 
 
-def get_git_diff(only: List[str], ignore: List[str]):
+def get_git_diff(only: List[str], ignore: List[str], branch: str = None):
     """
     Returns the git diff as a string
     If ignore is specified, diffs all files except those with those extensions
@@ -12,12 +12,12 @@ def get_git_diff(only: List[str], ignore: List[str]):
     if ignore:
         print(f"Diffing ALL files except those with extensions {ignore}")
         files = " ".join(f":(exclude)'**/*{ext}'" for ext in ignore)
-        command = f"git diff -- . {files}"
+        command = f"git diff {branch + ' -- ' if branch else ''}. {files}"
     elif only:
         print(f"Diffing exclusively files with extensions {only}")
         files = " ".join(f"'**/*{ext}'" for ext in only)
-        command = f"git diff -- {files}"
+        command = f"git diff {branch + ' -- ' if branch else ''}{files}"
     else:
         print(f"Diffing all files (no extensions specified))")
-        command = "git diff"
+        command = f"git diff {branch if branch else ''}"
     return run_shell_command(command)
