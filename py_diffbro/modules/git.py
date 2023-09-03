80fc77085d572cd1ef767f12663794632efdef1e
qwukker <minor7addfloortom@gmail.com>
2023-09-03 12:23:52 -0500

    update get_git_diff to build up command one by one

--- py_diffbro/modules/git.py
+++ py_diffbro/modules/git.py
@@ -9,15 +9,27 @@ def get_git_diff(only: List[str], ignore: List[str], branch: str = None):
     If only is specified, only diffs files with those extensions
     If neither are specified, diffs all files
     """
+
+    command = "git diff"
+
+    if branch:
+        command += f" {branch}"
+
+    if only or ignore:
+        command += " -- ."
+
     if ignore:
         print(f"Diffing ALL files except those with extensions {ignore}")
         files = " ".join(f":(exclude)'**/*{ext}'" for ext in ignore)
-        command = f"git diff {branch + ' ' if branch else ''}-- . {files}"
+        # command = f"git diff -- . {files}"
+        command += f" {files}"
     elif only:
         print(f"Diffing exclusively files with extensions {only}")
         files = " ".join(f"'**/*{ext}'" for ext in only)
-        command = f"git diff {branch + ' ' if branch else ''}-- {files}"
+        command += f" {files}"
     else:
         print(f"Diffing all files (no extensions specified))")
-        command = f"git diff {branch + ' ' if branch else ''}--"
+
+    print("Ending Command", command)
+
     return run_shell_command(command)
